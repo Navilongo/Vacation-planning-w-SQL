d2559637a28bfe2506aa1f92134a6e8df69b0496
Navilongo <navilongo@gmail.com>
2021-04-17 22:47:48 -0700

    Finally got app to work properly and tested precipitation

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# 1. Import 
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -10,25 +8,22 @@ from flask import Flask, jsonify
 
 engine = create_engine("sqlite:///hawaii.sqlite")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-# 2. Create an app
+
 app = Flask(__name__)
 
-
-# 3. Define static routes
 @app.route("/")
 def index():
-    return ("All routes in API <br/>"
-    f"/api/v1.0/precipitation"
-    f"/api/v1.0/stations"
-    f"/api/v1.0/tobs"
-    f"/api/v1.0/<start>"
-    f"/api/v1.0/<start>/<end>"
+    return ("All routes in API:<br/>"
+    f"/api/v1.0/precipitation<br/>"
+    f"/api/v1.0/stations<br/>"
+    f"/api/v1.0/tobs<br/>"
+    f"/api/v1.0/start"
+   
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -37,11 +32,13 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
 
-   precipitation = []
+    precipitation = []
     for date, prcp in results:
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
         precipitation.append(prcp_dict)
-        return jsonify(precipitation)
+    return jsonify(precipitation)
 
+if __name__ == '__main__':
+    app.run(debug=True)
